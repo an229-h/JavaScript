078cfb9f5a4e9cec2c3277d7c0ae65e613224238
Hari Om Verma <hariom229@hotmail.com>
2026-05-30 13:30:53 +0530

    enhanced error handling in script.js, fixed relocateCar function, optimized getStatus and availableSlots

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ let parkingLot = [
     { slot: 6, occupied: false, vehicle: null },
 ];
 
-
 function checkVehicle(carNumber) {
+    if(!carNumber){
+        return "Invalid Car Number Entered"
+    }
     for (let slot of parkingLot) {
         if (slot.vehicle === carNumber) {
             return slot;
@@ -18,6 +20,9 @@ function checkVehicle(carNumber) {
 }
 
 function parkCar(carNumber) {
+    if(!carNumber){
+        return "Invalid Car Number Entered"
+    }
     let exist = checkVehicle(carNumber)
     if (exist === false) {
         //Proceed to park
@@ -35,20 +40,23 @@ function parkCar(carNumber) {
 }
 
 function removeCar(carNumber) {
-    if(checkVehicle(carNumber)=== false) {
+    if(!carNumber){
+        return "Invalid Car Number Entered"
+    }
+    let vehicle = checkVehicle(carNumber)
+    if(vehicle=== false) {
         return `Car ${carNumber} not found`
     } else{
-        for(let slot of parkingLot) {
-            if(slot.vehicle===carNumber){
-                slot.vehicle=null;
-                slot.occupied=false;
-                return `Car ${carNumber} has been remove from slot ${slot.slot}`
-            }
-        }
+        vehicle.vehicle=null;
+        vehicle.occupied=false;
+        return `Car ${carNumber} has been removed from slot ${vehicle.slot}`
     }
 }
 
 function searchCar(carNumber) {
+    if(!carNumber){
+        return "Invalid Car Number Entered"
+    }
     let vehicleLocation = checkVehicle(carNumber);
     if(vehicleLocation===false){
         return `car ${carNumber} not found`
@@ -57,44 +65,61 @@ function searchCar(carNumber) {
     }
 }
 
-function getStatus(){
-    for(let slot of parkingLot){
-        if(slot.occupied===true){
-            console.log(`slot:${slot.slot}\nstatus:occupied\nvehicle:${slot.vehicle}`)
-        } else {
-            console.log(`slot:${slot.slot}\nstatus:empty`)
-        }
+function getStatus() {
+  let output = "";
+  for (let slot of parkingLot) {
+    if (slot.occupied === true) {
+      output += `slot:${slot.slot}
+status:occupied
+vehicle:${slot.vehicle}
+
+`;
+    } else {
+      output += `slot:${slot.slot}
+status:empty
+
+`;
     }
+  }
+  return output;
 }
 
 function availableSlots(){
+    let count=0
+    let output=""
     for(let slot of parkingLot){
         if(slot.occupied===false){
-            console.log(slot.slot)
+            output+=`${slot.slot}\n`
+            count++;
         }
     }
+    return `Total available slots - ${count}\n${output}`
 }
 
-function relocateCar(carNumber, newSlot){
-    let exist = checkVehicle(carNumber)
-    if(exist===false){
-        return `${carNumber} doesn't exist in our parking lot`
-    } else {
-        let relocated;
-        for(let slot of parkingLot){
-            if(slot.slot===newSlot && slot.occupied===false){
-                slot.occupied=true;
-                slot.vehicle=carNumber;
-                relocated = true;
-            }
-        }
-        for(let slot of parkingLot){
-            if(slot.slot==exist.slot && relocated===true){
-                slot.occupied=false;
-                slot.vehicle=null; 
-                return  'Car relocated successfully'
-            }
-        }
-        return "Car hasn't been relocated"
+function relocateCar(carNumber, newSlot) {
+    if(!carNumber || !newSlot){
+        return "Invalid Car Number Entered"
     }
+    let exist = checkVehicle(carNumber)
+    if (exist === false) {
+        return `${carNumber} doesn't exist in our parking lot`
+    } else if (newSlot === exist.slot) {
+        return "Vehicle is already in that slot"
+    } else {
+        let relocated = false;
+        for (let slot of parkingLot) {
+            if (slot.slot === newSlot && slot.occupied === false) {
+                slot.occupied = true;
+                slot.vehicle = carNumber;
+                relocated = true;
+                break;
+            }
+        }
+        if (relocated) {
+            exist.occupied = false;
+            exist.vehicle = null;
+            return "Car relocated successfully"
+        }
+    }
+    return "Car hasn't been relocated"
 }
